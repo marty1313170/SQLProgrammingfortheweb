57b8ea750d03beb4f962c4cef413c3d8aaa9a7ff
marty1313170 <martin.han1@education.nsw.gov.au>
2026-09-14 09:53:02 +1000

    Finished school.db

--- lesson1_connect.py
+++ lesson1_connect.py
@@ -1,2 +1,24 @@
 import sqlite3
 
+connection = sqlite3.connect("school.db")
+
+print("Database connected")
+
+connection.close()
+
+print("Hello you are opening an connection to the database")
+print("Database closed?")
+
+"""
+- What is the difference between Python and SQLite?
+
+SQlite is a database engine that runs data in a file and runs SQL while Python is a programming language, which talks with SQLite
+
+- What file was created when the script ran?
+
+school.db was created
+
+- What does the connection do?
+
+It creates the file and opens an connection to allow python to talk to the data.
+"""
