a3ea9b037080a8487996c9430e56d4b056743255
marty1313170 <martin.han1@education.nsw.gov.au>
2026-09-14 10:16:53 +1000

    Lesson 2 started

---
+++ lesson2_create_table.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+connection = sqlite3.connect("school.db")
+
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    year_group INTEGER,
+    favourite_subject TEXT
+)
+""")
+
+cursor.execute("DELETE FROM students")
+
+
+
+cursor.execute("INSERT INTO students (name, year_group, favourite_subject) VALUES (?, ?)", ("James", 10, "English"))
+cursor.execute("INSERT INTO students (name, year_group,  favourite_subject) VALUES (?, ?)", ("Leo", 12, "Math"))
+cursor.execute("INSERT INTO students (name, year_group  , favourite_subject) VALUES (?, ?)", ("Jimmy", 8, "Science"))
+
+
+
+connection.commit()
+
+connection.close()
+
+"""
+Create the script shown above.
+Run it in VS Code.
+Change one name value and run it again.
+Discuss why IF NOT EXISTS is useful.
+
+IF NOT EXISTS is useful because it ensures that you don't create a duplicate table with the same name
+
+Identify which column is the primary key.
+id is the primary key.
+
+"""
+
+
+"""
+Exit Check
+
+Why do we use commit()?
+
+To keep the changes we made to the database so it doesn't revert
+
+What does PRIMARY KEY mean?
+
+It is the unique key given to each record to prevent duplicates and maintain integrity
+"""
