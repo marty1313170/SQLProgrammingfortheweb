97bf5b84692107939e5c9c72357f532b98d096b3
marty1313170 <martin.han1@education.nsw.gov.au>
2026-09-17 14:45:57 +1000

    initial commit

--- lesson2_create_table.py
+++ lesson2_create_table.py
@@ -4,13 +4,14 @@ connection = sqlite3.connect("school.db")
 
 cursor = connection.cursor()
 
+
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    year_group INTEGER,
-    favourite_subject TEXT,
-    detentions INTEGER
+    year_group INTEGER
+   
 )
 """)
 
@@ -18,9 +19,9 @@ cursor.execute("DELETE FROM students")
 
 
 
-cursor.execute("INSERT INTO students (name, year_group, favourite_subject, detentions) VALUES (?, ?)", ("James", 10, "English", 0))
-cursor.execute("INSERT INTO students (name, year_group,  favourite_subject, detentions) VALUES (?, ?)", ("Leo", 12, "Math", 2))
-cursor.execute("INSERT INTO students (name, year_group  , favourite_subject , detentions) VALUES (?, ?)", ("Jimmy", 8, "Science", 19))
+cursor.execute("INSERT INTO students (name, year_group) VALUES (?, ?)", ("James", 10, ))
+cursor.execute("INSERT INTO students (name, year_group) VALUES (?, ?)", ("Leo", 12, ))
+cursor.execute("INSERT INTO students (name, year_group  ) VALUES (?, ?)", ("Jimmy", 8,))
 
 
 
